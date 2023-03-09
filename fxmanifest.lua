4f2aabf5481cb0d5c371d881909ab6fe15113431
sajjadmrx <sajjadzibaee102@gmail.com>
2023-03-09 15:56:35 +0330

    added React

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,16 +4,25 @@ author 'test'
 version '1.0.0'
 description 'manage shop'
 lua54 'yes'
+
+
 shared_script '@es_extended/imports.lua'
 
-ui_page('/client/ui/index.html')
+client_scripts({
+})
+
+server_script({
+    '@oxmysql/lib/MySQL.lua',
+    'locales/*.lua',
+    'config.lua',
+    'server/*.lua'
+})
+
+ui_page('client/ui/build/index.html')
 
 files({
-    '/client/ui/index.html',
-    '/client/ui/**/*.js',
+    'client/ui/build/index.html',
+    'client/ui/build/static/js/*.js',
 })
 
-client_scripts({
-    '/client/ui/index.html',
-    '/client/ui/**/*.js',
-})
+dependency 'es_extended'
