ae5f308a0a42beeb45561ca6daa40718639b3e6b
MoskalykA <100430077+MoskalykA@users.noreply.github.com>
2023-03-18 16:14:19 +0100

    refactor: improvement of the French translation

---
+++ locales/fr.lua
@@ -0,0 +1,10 @@
+Locales['fr'] = {
+  ['open_menu'] = 'appuyez sur [%s] pour ouvrir le menu',
+  ["shop"] = 'boutique',
+  ["not_enough_money"] = "Vous n'avez pas assez d'argent.",
+  ["enough_money_in_bank"] = "Vous avez assez d'argent sur votre compte bancaire.",
+  ["invalid_product"] = "Produit invalide",
+  ["number_exceeded"] = "Le nombre d'articles dépasse la limite autorisée.",
+  ["access_denied"] = "ACCÈS REFUSÉ",
+  ['invalid_request'] = "Requête invalide"
+}
